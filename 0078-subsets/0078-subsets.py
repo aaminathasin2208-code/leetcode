abc1e81b78f841b0270a6f51f19b5f49ddba9b3a
aaminathasin2208-code <aamina.thasin2208@gmail.com>
2026-04-25 21:04:57 +0530

    Time: 0 ms (100%), Space: 12.6 MB (27.94%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,14 @@
+class Solution:
+    def subsets(self, nums):
+        result = []
+
+        def backtrack(start, path):
+            result.append(path[:])  # add current subset
+            
+            for i in range(start, len(nums)):
+                path.append(nums[i])       # choose
+                backtrack(i + 1, path)    # explore
+                path.pop()                # undo (backtrack)
+
+        backtrack(0, [])
+        return result
